9372b6d3c7d151af25a348969804f65d4f4894cd
nguyenpatz <nguyen79248@st.vimaru.edu.vn>
2022-03-03 08:39:45 +0700

    update footer

--- view/footer.php
+++ view/footer.php
@@ -1,9 +1,112 @@
-<footer>
+<footer id="footer">
+    	<div class="footer-ship">
+    		<ul class="container footer-ship">
+    			<li>
+    				<i class="fa fa-pencil-square-o"></i>
+    				<strong> Chiết khấu ưu đãi</strong>
+    				<p>Chưa biết viết gì</p>
+    			</li>
 
-</footer>
+    			<li>
+    				<i class="fa fa-check-circle-o"></i>
+    				<strong>Chất lượng dịch vụ</strong>
+    				<p>Chưa biết viết gì</p>
+    			</li>
+
+    			<li>
+    				<i class="fa fa-clock-o"></i>
+    				<strong>Thời gian giao hàng</strong>
+    				<p>Chưa biết viết gì</p>
+    			</li>
+
+    			<li>
+    				<i class="fa fa-money"></i>
+    				<strong> Giá cả phải chăng</strong>
+    				<p>Chưa biết viết gì</p>
+    			</li>
+
+    			<li>
+    				<i class="fa fa-cubes"></i>
+    				<strong>Dịch vụ khách hàng</strong>
+    				<p>Chưa biết viết gì</p>
+    			</li>
+    		</ul>
+    	</div>
+    	<div class="dichvu">
+    		<h2> DỊCH VỤ VẬN TẢI HÀNG KHÔNG</h1>
+    		<ul>
+    			<li><a href="#">Dịch vụ vận chuyển hàng không giá rẻ</a></li>
+    			<li><a href="#">Dịch vụ vận chuyển hàng không Campuchia uy tín,chuyên nghiệp</a></li>
+    			<li><a href="#">Báo giá hàng không xuất nhập khẩu</a></li>
+    		</ul>
+    	</div>
+    	<div class ="dichvu">
+    		<h2>DỊCH VỤ VẬN TẢI</h2>
+    		<ul>
+    			<li><a href="#">Dịch vụ vận tải từ Hải Phòng đi các tỉnh</a></li>
+    			<li><a href="#">Dịch vụ vận tải từ Hải Phòng đi Hồ Chí Minh</a></li>
+    			<li><a href="#">Dịch vụ vận tải từ Hải Phòng Lào</a></li>
+    		</ul>
+    	</div>
+    	<div class="dichvu">
+    		<h2>DỊCH VỤ HẢI QUAN</h2>
+    		<ul>
+    			<li><a href="#">Dịch vụ cod tại Hải Phòng</a></li>
+    			<li><a href="#">Dịch vụ cod tại Hà Nội</a></li>
+    		</ul>
+    	</div>
+
+
+    	<div class="container">
+    	<div><img src="logo.png" alt=""></div>
+    		<div class="noidung">
+    				<img src="logo.png" alt="">
+    				<p>
+    					CÔNG TY TNHH DỊCH VỤ LOGISTICS & THƯƠNG MẠI HOÀNG MINH <br>
+    				</p>
+    			</div class ="noidung-link">
+    				<ul>
+    					<li><a href="#">Trang Chủ</a></li>
+    					<li><a href="#">Thông tin liên hệ</a></li>
+    					<li><a href="#">Dịch vụ</a></li>
+    					<li><a href="#">Tư vấn hỏi đáp</a></li>
+    					<li><a href="#">Liên hệ</a></li>
+    				</ul>
+    				<ul class="social-icon">
+                 <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                 <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                 <li><a href=""><i class="fa fa-instagram"></i></a></li>
+                 <li><a href=""><i class="fa fa-youtube"></i></a></li>
+             </ul>
+    			</div>
+    			<div class="noidung-contact">
+    				<h2> Thông tin liên hệ</h2>
+    				<ul class="infor">
+    					<li>
+    						<span><i class="fa fa-map-maker"></i></span>
+    						<span>21/103 Lê Lai <br>
+    							Quận Ngô Quyền,Thành phố Hải Phòng <br>
+    							Việt Nam</span>
+    					</li>
+    					<li>
+    						<span><i class="fa fa-phone"></i></span>
+    						<span> 0903.455.227</span>
+    					</li>
+    					<li>
+    						<span><i class="fa fa-envelope"></i></span>
+    						<span> lexuanhung.hoangminh@gmail.com </span>
+    					</li>
+    				</ul>
+    				<img src="bocongthuong.png" alt="">
+    			</div>
+    		</div>
+    	 </div>
+    </footer>
+	</div>
+</body>
 <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-</body>
+</html>
