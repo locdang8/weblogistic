cab6dd7cc222b54071e0d1b9dcfdf2e860a1b3ee
locdang <locdangkvh892k@gmail.com>
2022-03-04 11:16:49 +0700

    loc

---
+++ view/tintuc.php
@@ -0,0 +1,145 @@
+<?php include "header.php"?>
+<link rel="stylesheet" href="../view/css/gioithieu.css">
+		<div class="news container-fluid">
+			<!-- image------------- -->
+			<div class="news-image text-center">
+				<img class="container-fluid mx-0 px-0"src="https://bestcargo.vn/wp-content/uploads/2018/05/van-chuyen-hang-hoa-quoc-te-chuyen-nghiep.jpg" alt="" >
+			</div>
+			<!-- --------------------- -->
+
+			<!-- data -->
+			<div class="news-data container-fluid mt-5">
+				<div class="news-data-title text-center">
+					<p class="h5 font-weight-bold border-bottom mb-0">Tin tức</p>
+				</div>
+				<div class="news-data-grid row mt-10">
+					<div class="col-md-8 border-right">
+						<div class="row">
+
+							<!-- bai viet .......   -->
+							<div class="col-6 news-data-item">
+								<div class="data-image">
+									<img class="container mx-0 px-0"src="https://bestcargo.vn/wp-content/uploads/2022/01/Van-chuyen-hang-hoa-Bac-Nam-bang-tau-hoa.jpeg" alt="">
+								</div>
+								<div class="data-note text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu</p>
+								</div>
+								<div class="data-title text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu Trong trường hợp nào doanh nghiệp xuất nhập...</p>
+								</div>
+							</div>
+
+							<div class="col-6 news-data-item">
+								<div class="data-image">
+									<img class="container mx-0 px-0"src="https://bestcargo.vn/wp-content/uploads/2022/01/Van-chuyen-hang-hoa-Bac-Nam-bang-tau-hoa.jpeg" alt="">
+								</div>
+								<div class="data-note text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu</p>
+								</div>
+								<div class="data-title text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu Trong trường hợp nào doanh nghiệp xuất nhập...</p>
+								</div>
+							</div>
+
+							<div class="col-6 news-data-item">
+								<div class="data-image">
+									<img class="container mx-0 px-0"src="https://bestcargo.vn/wp-content/uploads/2022/01/Van-chuyen-hang-hoa-Bac-Nam-bang-tau-hoa.jpeg" alt="">
+								</div>
+								<div class="data-note text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu</p>
+								</div>
+								<div class="data-title text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu Trong trường hợp nào doanh nghiệp xuất nhập...</p>
+								</div>
+							</div>
+
+							<div class="col-6 news-data-item">
+								<div class="data-image">
+									<img class="container mx-0 px-0"src="https://bestcargo.vn/wp-content/uploads/2022/01/Van-chuyen-hang-hoa-Bac-Nam-bang-tau-hoa.jpeg" alt="">
+								</div>
+								<div class="data-note text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu</p>
+								</div>
+								<div class="data-title text-center">
+									<p>Dịch vụ tư vấn xuất nhập khẩu Trong trường hợp nào doanh nghiệp xuất nhập...</p>
+								</div>
+							</div>
+							<!-- --------------- -->
+
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="menu">
+							<div class="menu-list">
+								<div class="menu-title">
+									<p class="h5">Dịch vụ vận tải hàng không</p>
+								</div>
+								<div class="menu-item">
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+								</div>
+							</div>
+
+							<div class="menu-list">
+								<div class="menu-title">
+									<p class="h5">Dịch vụ vận tải hàng không</p>
+								</div>
+								<div class="menu-item">
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+								</div>
+							</div>
+
+							<div class="menu-list">
+								<div class="menu-title">
+									<p class="h5">Dịch vụ vận tải hàng không</p>
+								</div>
+								<div class="menu-item">
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+								</div>
+							</div>
+
+							<div class="menu-list">
+								<div class="menu-title">
+									<p class="h5">Dịch vụ vận tải hàng không</p>
+								</div>
+								<div class="menu-item">
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+									<li><a href="">Dịch vụ vận tải hàng không đi Brazil giá rẻ</a></li>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+				<div class="phan-trang container m-5 justify-content-center row">
+					<div class="phan-trang-item justify-content-center"><a href="">1</a></div>
+					<div class="phan-trang-item justify-content-center"><a href="">2</a></div>
+					<div class="phan-trang-item justify-content-center"><a href="">3</a></div>
+                    <div class="phan-trang-item justify-content-center"><a href="">4</a></div>
+                    <div class="phan-trang-item justify-content-center"><a href="">5</a></div>
+					<div class="phan-trang-item justify-content-center"><a href="">...</a></div>
+                    <div class="phan-trang-item justify-content-center"><a href="">7</a></div>
+					<div class="phan-trang-item justify-content-center"><a href="">8</a></div>
+					<div class="phan-trang-item justify-content-center"><a href="">9</a></div>
+				</div>
+			</div>
+		</div>
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+	</body>
+</html>
