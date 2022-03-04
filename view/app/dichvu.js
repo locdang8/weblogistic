cc71f2467f3e4373c94795d02b750d9f1f685198
nguyenpatz <nguyen79248@st.vimaru.edu.vn>
2022-03-05 06:20:18 +0700

    update file dichvu.js id list-set

--- view/app/dichvu.js
+++ view/app/dichvu.js
@@ -11,18 +11,24 @@ let dichvu_array = [
 
 const listMini = document.querySelector(".list-mini");
 // listSet để hiển thị menu dịch vụ
-const listSet = document.querySelector(".list-set");
+const listSet = document.querySelector("#list-set");
 // dropdown để bật tắt menu dịch vụ
 const dropDown = document.querySelector(".dropdown");
 window.addEventListener("DOMContentLoaded", function () {
-  let ten_dv = dichvu_array
+  let ten_dv = dichvu_array;
     .map(function (element) {
       return `<li><a href="">${element.ten}</a></li>`;
     })
     .join("");
 
+    let ten_dv2 = dichvu_array;
+    .map(function (element) {
+      return `<a class="dropdown-item" href="#">${element.ten}</a>`
+    })
+    .join("");
+
   listMini.innerHTML = ten_dv;
-  listSet.innerHTML = ten_dv;
+  listSet.innerHTML = ten_dv2;
 });
 
 dropDown.addEventListener("click", function () {
