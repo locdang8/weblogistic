b2fba164d44a0a33b6dfc84158e30d8e4f045f2d
nguyenpatz <nguyen79248@st.vimaru.edu.vn>
2022-03-07 09:00:37 +0700

    update element menu

--- view/app/dichvu.js
+++ view/app/dichvu.js
@@ -15,14 +15,12 @@ const listSet = document.querySelector("#list-set");
 // dropdown để bật tắt menu dịch vụ
 const dropDown = document.querySelector(".dropdown");
 window.addEventListener("DOMContentLoaded", function () {
-  let ten_dv = dichvu_array;
-  dichvu_array.map(function (element) {
+  let ten_dv = dichvu_array.map(function (element) {
       return `<li><a href="">${element.ten}</a></li>`;
     })
     .join("");
 
-    let ten_dv2 = dichvu_array;
-    dichvu_array.map(function (element) {
+    let ten_dv2 = dichvu_array.map(function (element) {
       return `<a class="dropdown-item" href="#">${element.ten}</a>`
     })
     .join("");
